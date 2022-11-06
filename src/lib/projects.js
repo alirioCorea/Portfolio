03b96f7da8a0623f1dabdef224b9f975da3fb9cc
Alirio Corea <coreaalirio@gmail.com>
2022-11-05 18:42:07 -0600

    Added other prokects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,6 +1,9 @@
 import projImg1 from '../assets/img/project-img1-white.png'
 import projImg2 from '../assets/img/project-img2-white.png'
 import projImg3 from '../assets/img/project-img3-white.png'
+import projImg4 from '../assets/img/project-img4-white.png'
+import projImg5 from '../assets/img/project-img5-white.png'
+import projImg6 from '../assets/img/project-img6-white.png'
 
 const projects = [
     {
@@ -17,6 +20,21 @@ const projects = [
       title: "Video Games",
       description: "Design & Development",
       imgUrl: projImg3,
+    },
+    {
+      title: "Business Form",
+      description: "Design & Development",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Business furniture",
+      description: "Design & Development",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Learning Project",
+      description: "Design & Development",
+      imgUrl: projImg6,
     },
   ];
 
